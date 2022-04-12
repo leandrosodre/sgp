83a4cce8ff3668fe4d035535081b257914bd4588
leandro.sodre <leandroaugustosodre@gmail.com>
2022-04-12 00:27:16 -0300

    ajuste telas

--- src/main/java/com/sgp/controllers/TaskController.java
+++ src/main/java/com/sgp/controllers/TaskController.java
@@ -11,6 +11,7 @@ import com.sgp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -50,4 +51,12 @@ public class TaskController {
         mv.addObject("sprints", sprints);
         return mv;
     }
+
+    @PostMapping(value = "/createTask")
+    public String createTask(final Task task) {
+        taskRepository.save(task);
+        return "redirect:/tasks";
+    }
+
+
 }
